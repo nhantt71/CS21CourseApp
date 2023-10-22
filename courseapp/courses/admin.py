30c215575f9e91df35923169c4b8305aa18ba090
nhan to <tonhanlk7103@gmail.com>
2023-10-22 15:04:40 +0700

    ThucHanhBuoi122/10/23

---
+++ courseapp/courses/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Category, Course
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name']
+    list_select_related = True
+    list_per_page = 200
+    list_max_show_all = 150
+    search_fields = ['name']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Course)
+
+
+
